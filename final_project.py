5da89f1127e42f436440c30857c6ff8ca2b533d3
Mercedes Garcia <mercedesgarcia@Mercedess-MacBook-Air.local>
2018-04-13 00:39:14 -0700

    Added monkey Code and Set up the rooms and animal functions
    I added the code for all the animals since its pretty much the same as my monkey code, if you guys don't like the code for you animal, you can totally change it up, my feelings won't get hurt, I promise!

--- final_project.py
+++ final_project.py
@@ -1,27 +1,117 @@
 #Authors: Joshua Howard, Eric Ybarra, Mercedes Garcia, William Barajas
 
 #CST 205 Final Project
-#animals = ['monkey','lion','bear','elephant']
+
 def playAnySound():
   file = pickAFile()
   sound = makeSound(file)
   play(sound)
-  
+def guesses(): #This function should keep track of the number of guesses the user has
+  #global guesses = 4
+  showInformation("You have x number of guesses")  
+
+def end(userInput):#The purpose of this function is to quit the game if the user were to input "exit".
+  while(userInput == "exit"):
+    if(userInput == "exit"):
+      print ""
+      printNow ("  Done.")
+      break  
+
+def userInput(): #This function will allow the user to traverse the zoo from differenct areas(1-4)
+  userInput = requestString("What area would you like to go? ")
+  printNow(userInput)
+  end(userInput)#Will end/exit game
+  if(userInput == "help"):#Will re-start game and provide instructions.
+    zooGame() 
+    printNow(userInput)  
+  if(userInput == "area1"):#Area1
+    area1()
+    printNow(userInput)
+  if(userInput == "area2"):#Area2
+    area2()   
+    printNow(userInput)
+  if(userInput == "area3"):#Area3
+    area3()
+    printNow(userInput)
+  if(userInput == "area4"):#Area4
+    area4()
+                   
 #This function will allow user to listen to sound clip and guess the animal.
 def monkey():
-  print "Listen Carefully to the following sound...."
-  playAnySound()# chosen file is "monkey.wav"
-  requestString("Guess the animal: ")
+  showInformation( "Listen Carefully to the following sound....")
+  playAnySound()# chose file "monkey.wav"
+  answer = requestString("Guess the animal: ")
   if (answer == 'monkey'):
-    print "Good job! It is correct!"
+    showInformation("Good job! It is correct!")
+  else:
+    showInformation("Sorry, you guessed wrong!")
+        
+def elephant():
+  showInformation( "Listen Carefully to the following sound....")
+  playAnySound()# chose file 
+  answer = requestString("Guess the animal: ")
+  if (answer == 'elephant'):
+    showInformation("Good job! It is correct!")
+  else:
+    showInformation("Sorry, you guessed wrong!")
+    
+def bear():
+  showInformation( "Listen Carefully to the following sound....")
+  playAnySound()# chose file 
+  answer = requestString("Guess the animal: ")
+  if (answer == 'bear'):
+    showInformation("Good job! It is correct!")
+  else:
+    showInformation("Sorry, you guessed wrong!")
+
+def lion():
+  showInformation( "Listen Carefully to the following sound....")
+  playAnySound()# chose file 
+  answer = requestString("Guess the animal: ")
+  if (answer == 'lion'):
+    showInformation("Good job! It is correct!")
+  else:
+    showInformation("Sorry, you guessed wrong!")
+#Area1
+def area1():
+  showInformation("Welcome to Area 1!")
+  monkey()
   
-  
-  
+
+#Area2
+def area2():
+  showInformation("Welcome to Area 2!")
+  elephant()
+
+#Area3
+def area3():
+  showInformation("Welcome to Area 3!")
+  bear()
+
+#Area4
+def area4():
+  showInformation("Welcome to Area 4!")
+  lion()
+
+
 def zooGame():
-  animals = [monkey,lion,elephant,bear]
+  print ""
+  print "    Welcome to 'Zoo Escape Game'" 
+  print ""
+  print "  Instructions: "
+  print ""
+  print "  4 animal have just escaped!"
+  print "  You are the zookeeper and you have to find and capture the animals to return them to their cages."
+  print "  You will traverse a map of the zoo by typing the name of the different area you want to go to." 
+  print "  There is Area1, Area 2, Area3, and Area 4"
+  print ""
+  print "  Once you go to an area you will be prompted to listen for an animal." 
+  print "  After you hear the animal sound, you have to guess which animal it is." 
+  print "  You are allowed a total of 2 wrong guesses before and you lose the game." 
+  print "  But if you guess all of the animals right within the allowed number of guesses you will win."
+  print ""
+  print "  1) Type 'help' to redisplay this introduction"
+  print "  2) Type 'exit' to quit at any time"
   
-  print "      Welcome to 'Zoo Escape'"
-  
-  #Testing again for William 
-  print "Testing"
+  userInput()
 
