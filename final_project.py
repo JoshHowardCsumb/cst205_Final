4bc462669d3a7f3bc872e0d57d5b68a63a7c5c64
Mercedes Garcia <mercedesgarcia@Mercedess-MacBook-Air.local>
2018-04-12 23:38:59 -0700

    playSound function

--- final_project.py
+++ final_project.py
@@ -1,11 +1,16 @@
 #Authors: Joshua Howard, Eric Ybarra, Mercedes Garcia, William Barajas
 
 #CST 205 Final Project
-
+#animals = ['monkey','lion','bear','elephant']
+def playAnySound():
+  file = pickAFile()
+  sound = makeSound(file)
+  play(sound)
+  
 #This function will allow user to listen to sound clip and guess the animal.
 def monkey():
-  print "Press play"
-  #sound clip
+  print "Listen Carefully to the following sound...."
+  playAnySound()# chosen file is "monkey.wav"
   requestString("Guess the animal: ")
   if (answer == 'monkey'):
     print "Good job! It is correct!"
